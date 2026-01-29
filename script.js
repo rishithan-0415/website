8aa47bc22198485d5092d1b6b3ce9d2357e6c076
Rishitha N <rishithangowda3@gmail.com>
2026-01-29 22:15:11 +0530

    deploy

--- script.js
+++
@@ -1,9 +0,0 @@
-// Scroll to section smoothly
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
-
-// Show alert
-function showAlert() {
-  alert("Admissions form coming soon!");
-}
